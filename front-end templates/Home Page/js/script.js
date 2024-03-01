9cc3c4324dad9401c88d505af2443e02e496583d
devfadyhany <dev.fadyhany@gmail.com>
2024-03-01 17:46:47 +0200

    Restructured files to seperate global form specific.

--- front-end templates/Home Page/js/script.js
+++ front-end templates/Home Page/js/script.js
@@ -1,19 +1,20 @@
 let toggleMenu = true;
-let header = document.querySelector("#Header")
-let menuBtn = document.querySelector("#menuBtn");
-let menu = document.querySelector("#myMenu");
 
-function menuToggler() {
+const MobileMenuToggler = () => {
+  let menu = document.querySelector("#myMenu");
+
   toggleMenu
     ? menu.classList.remove("hide-menu")
     : menu.classList.add("hide-menu");
   toggleMenu = !toggleMenu;
-}
+};
 
-function navbarScroll(){
+const NavbarChangeOnScroll = () => {
+  let header = document.querySelector("#Header");
+
   if (window.scrollY > 10) {
-    header.classList.add("navbar-scroll")
-  }else {
-    header.classList.remove("navbar-scroll")
+    header.classList.add("navbar-scroll");
+  } else {
+    header.classList.remove("navbar-scroll");
   }
-}
+};
