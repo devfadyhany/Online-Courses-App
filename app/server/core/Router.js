b49c17cbbf5a9756fc1a9bf69d4766ccce8398ae
devfadyhany <dev.fadyhany@gmail.com>
2024-06-22 19:19:16 +0300

    Final version of back-end API

--- app/server/core/Router.js
+++ app/server/core/Router.js
@@ -1,8 +1,6 @@
 import express from "express";
 import userRouter from "../router/UserRouter.js";
 import courseRouter from "../router/CourseRouter.js";
-import adminRouter from "../router/AdminRouter.js";
-import commentRouter from "../router/CommentRouter.js";
 import enrollRouter from "../router/EnrollRouter.js";
 import videoRouter from "../router/VideoRouter.js";
 
@@ -10,8 +8,6 @@ const mainRouter = express.Router();
 
 mainRouter.use("/user", userRouter);
 mainRouter.use("/course", courseRouter);
-mainRouter.use("/admin", adminRouter);
-mainRouter.use("/comment", commentRouter);
 mainRouter.use("/enroll", enrollRouter);
 mainRouter.use("/video", videoRouter);
 
